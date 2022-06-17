8c9f7a90f8c3f0d59139ba13a205d13f5086d24b
ConstantinSrv <ltd7852@gmail.com>
2022-06-17 12:41:49 -0700

    fixed the magic number

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -12,6 +12,7 @@
 using namespace std;
 
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
+const double DEVIATION = 1e-6;
 
 string ReadLine() {
     string s;
@@ -87,7 +88,7 @@ public:
         
         sort(matched_documents.begin(), matched_documents.end(),
              [](const Document& lhs, const Document& rhs) {
-                if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
+                if (abs(lhs.relevance - rhs.relevance) < DEVIATION) {
                     return lhs.rating > rhs.rating;
                 } else {
                     return lhs.relevance > rhs.relevance;
